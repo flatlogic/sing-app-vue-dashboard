da0aee069657f09800de86289313972ec01bfeed
shoudaos <shoudaos92@gmail.com>
2019-10-11 13:38:39 +0300

    fix(config): fix rickshaw issue in production

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  publicPath: 'sing-app-vue/',
   productionSourceMap: false,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        // this is an workaround of jquery requirement for the vue-bootstrap-slide library
-        "jquery": path.join(__dirname, "./jqueryStub.js")
-      }
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      const terserWebpackPlugin = config.optimization.minimizer[0];
+      const terserOptions = terserWebpackPlugin.options.terserOptions;
+      terserOptions.mangle = {
+        reserved: ['$super']
+      };
     }
+    config.resolve.alias["jquery"] = path.join(__dirname, "./jqueryStub.js");
   }
 };
