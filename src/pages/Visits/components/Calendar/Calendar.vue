b07fe5092e347a747e9cd9f54e6611b5275bb725
shoudaos <shoudaos92@gmail.com>
2019-10-14 15:31:21 +0300

    fix(Calendar): fix styles in production

--- src/pages/Visits/components/Calendar/Calendar.vue
+++ src/pages/Visits/components/Calendar/Calendar.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-calendar is-expanded :attributes="attrs">
-    <div slot="day-popover" slot-scope="{dayTitle, attributes}">
-      <header class="fs-sm text-light fw-semi-bold text-center">
-        {{ dayTitle }}
-      </header>
-      <v-popover-row :attribute="attr" class="text-white" v-for="attr in attributes" :key="attr.key">
-        <p v-if="attr.popover.title" class="fw-normal m-0">{{attr.popover.title}}</p>
-        <p v-if="attr.popover.title" class="fw-thin m-0">{{attr.popover.description}}</p>
-        <a v-if="attr.popover.link" class="fw-thin m-0" :href="attr.popover.href">{{attr.popover.link}}</a>
-      </v-popover-row>
-    </div>
-  </v-calendar>
+  <span class="sing-calendar">
+    <v-calendar is-expanded :attributes="attrs">
+      <div slot="day-popover" slot-scope="{dayTitle, attributes}">
+        <header class="fs-sm text-light fw-semi-bold text-center">
+          {{ dayTitle }}
+        </header>
+        <v-popover-row :attribute="attr" class="text-white" v-for="attr in attributes" :key="attr.key">
+          <p v-if="attr.popover.title" class="fw-normal m-0">{{attr.popover.title}}</p>
+          <p v-if="attr.popover.title" class="fw-thin m-0">{{attr.popover.description}}</p>
+          <a v-if="attr.popover.link" class="fw-thin m-0" :href="attr.popover.href">{{attr.popover.link}}</a>
+        </v-popover-row>
+      </div>
+    </v-calendar>
+  </span>
 </template>
 
 <script>
