e6cad9204080ec24a6acebd0c085b243783ac87c
shoudaos <shoudaos92@gmail.com>
2019-12-27 18:33:02 +0300

    feat(Chat Page): new message functionality

--- src/pages/Chat/components/ChatDialog/ChatDialog.vue
+++ src/pages/Chat/components/ChatDialog/ChatDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="d-flex flex-column">
     <header class="chat-dialog-header">
       <div>
         <h5 class="fw-normal mb-0">{{activeChatUser.name}} {{activeChatUser.surname}}</h5>
@@ -7,7 +7,7 @@
       </div>
       <i class="info-icon la la-ellipsis-v"></i>
     </header>
-    <div class="chat-dialog-body">
+    <div class="chat-dialog-body" ref="chatDialogBody">
       <div v-for="(part, i) of dialogParts" :key="i">
         <div v-if="isTimeDivider(part)" class="dialog-divider">
           {{part[0]}}
@@ -17,19 +17,33 @@
         </div>
       </div>
     </div>
+    <form class="chat-section new-message mb-0" @submit="newMessageRequest({dialogId: dialog.id, message: newMessage})">
+      <b-button class="attachment" variant="transparent p-0"><i class="la la-plus"></i></b-button>
+      <b-input v-model="newMessage" placeholder="Type Your Message"></b-input>
+      <b-button variant="danger" class="px-4 new-message-btn" type="submit">
+        <Loader v-if="sendingMessage"></Loader>
+        <span v-else>Send</span>
+      </b-button>
+    </form>
   </div>
 </template>
 
 <script>
-  import {mapState} from 'vuex';
+  import {mapState, mapActions} from 'vuex';
   import moment from 'moment';
   import ChatMessage from './ChatMessage';
+  import Loader from '../../../../components/Loader/Loader';
 
   export default {
     name: 'ChatDialog',
-    components: {ChatMessage},
+    components: {ChatMessage, Loader},
+    data() {
+      return {
+        newMessage: ''
+      }
+    },
     computed: {
-      ...mapState('chat', ['activeChatUser', 'user']),
+      ...mapState('chat', ['activeChatUser', 'user', 'sendingMessage']),
       dialog() {
         return this.user.dialogs.find(d => d.withId === this.activeChatUser.id);
       },
@@ -61,7 +75,19 @@
         return firstLetter + substring;
       },
     },
+    watch: {
+      dialogParts() {
+        this.newMessage = '';
+        setTimeout(() => {
+          this.$refs.chatDialogBody.scrollTop = this.$refs.chatDialogBody.scrollHeight;
+        })
+      }
+    },
+    mounted() {
+      this.$refs.chatDialogBody.scrollTop = this.$refs.chatDialogBody.scrollHeight;
+    },
     methods: {
+      ...mapActions('chat', ['newMessageRequest']),
       shortCalendarDate(date) {
         return moment(date).calendar(null, {
           sameDay: '[Today]',
@@ -76,12 +102,8 @@
         return typeof dialogPart[0] === 'string';
       },
       showAvatar(dialogPart, message, index) {
-        if (index === 0 || dialogPart[index - 1].owner !== message.owner) {
-          return true;
-        } else {
-          return false;
-        }
-      }
+        return index === 0 || dialogPart[index - 1].owner !== message.owner;
+      },
     }
   }
 </script>
--- src/store/chat.js
+++ src/store/chat.js
@@ -1,4 +1,5 @@
 import {user, groups, users} from '../pages/Chat/mock';
+import moment from 'moment';
 
 export default {
   namespaced: true,
@@ -6,17 +7,41 @@ export default {
     user,
     groups,
     users,
-    activeChatUser: users[0]
+    activeChatUser: users[0],
+    sendingMessage: false,
   },
   mutations: {
     SET_ACTIVE_USER(state, payload) {
       state.activeChatUser = state.users.find(u => u.id === payload);
     },
-
+    NEW_MESSAGE_SUCCESS(state, payload) {
+      let dialog = state.user.dialogs.find(d => d.id === payload.dialogId);
+      dialog.messages.push(payload.message);
+      state.sendingMessage = false;
+    },
+    NEW_MESSAGE_REQUEST(state) {
+      state.sendingMessage = true;
+    }
   },
   actions: {
     setActiveUser({commit}, payload) {
       commit('SET_ACTIVE_USER', payload)
     },
+    newMessageRequest({dispatch, commit}, payload) {
+      if (!payload.message) return;
+      commit('NEW_MESSAGE_REQUEST');
+      setTimeout(() => {
+        let message = {
+          id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+          text: payload.message,
+          timestamp: moment(),
+          owner: true
+        };
+        dispatch('newMessageSuccess', {dialogId: payload.dialogId, message});
+      }, 1000)
+    },
+    newMessageSuccess({commit}, payload) {
+      commit('NEW_MESSAGE_SUCCESS', payload);
+    }
   },
 };
