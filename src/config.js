bdc3ded05800a83fc92a8d6ebe3920e80f11807e
Yeliazar <erik.ruzhakov@mail.ru>
2022-08-04 18:59:21 +0300

    updated config

--- src/config.js
+++ src/config.js
@@ -24,12 +24,12 @@ export default {
       ['fifth', '#20AE8C']
     ],
     navbarColors: [
-      ['test', '#ffffff'],
-      ['test2', '#E2E7EC'],
-      ['test3', '#C9D1FB'],
-      ['test4', '#C1C3CF'],
-      ['test5', '#0C2236'],
-      ['test6', '#6FB0F9']
+      ['first', '#ffffff'],
+      ['second', '#E2E7EC'],
+      ['third', '#C9D1FB'],
+      ['fourth', '#C1C3CF'],
+      ['fifth', '#0C2236'],
+      ['sixth', '#6FB0F9']
     ],
     colors: {
       dark: "#002B49",
